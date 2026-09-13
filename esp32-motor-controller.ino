05e78025f5ba9da7d15da0653bc6cb57cd929b2c
elaine <elaine6367.3@gmail.com>
2026-09-13 13:03:49 +0000

    added (untested filler code)
    hope hope hope i lowkey won't be back at brissy to test the code, gonna
    have to wait until saturday-ish

--- esp32-motor-controller.ino
+++ esp32-motor-controller.ino
@@ -1,8 +1,27 @@
 
-static void() {
-	
+const int motorEn = 22;
+const int motorIn1 = 2;
+const int motorIn2 = 4;  
+
+void setup() {
+  // these likely aren't the right pin names
+  // replace these with whatever pins i decide to use on the esp
+  pinMode(motorIn1, OUTPUT);
+  pinMode(motorIn2, OUTPUT);
+  pinMode(motorEn, OUTPUT);
+  
+  ledcAttach(motorEn, 5000, 8);
 }
 
-static loop() {
-	
+void loop() { // 'proof of concept' ahh, just untested filler essentially
+  // forward
+  digitalWrite(motorIn1, HIGH);
+  digitalWrite(motorIn2, LOW);
+  ledcWrite(motorEn, 200); // speed ranges from 0-255
+  delay(2000);
+
+  // stop
+  digitalWrite(motorIn1, LOW);
+  digitalWrite(motorIn2, LOW);
+  delay(1000);
 }
