197d1ad78b19c29b33949fb02400f8e1e081a268
elaine <elaine6367.3@gmail.com>
2026-09-24 17:14:30 +1000

    bluetooth to start and stop 1 motor

--- esp32-motor-controller.ino
+++ esp32-motor-controller.ino
@@ -1,27 +1,112 @@
+// bluetooth setup
+#include "BluetoothSerial.h"
 
-const int motorEn = 22;
-const int motorIn1 = 2;
-const int motorIn2 = 4;  
+#if !defined(CONFIG_BT_ENABLED)  || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please  run `make menuconfig` to and enable it
+#endif
 
+
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error  Serial Bluetooth not available or not enabled. It is only available for the ESP32  chip.
+#endif
+
+// variables and shit
+BluetoothSerial SerialBT;
+String text = ""; // io buffer of some sort
+enum BT { CTL_ON, CTL_OFF, UNDEFINED }; // input states, increase in future
+int BT = CTL_OFF; // default state
+
+// motor setup
+int motor1Pin1 = 27; 
+int motor1Pin2 = 26; 
+int enable1Pin = 14; 
+ 
+// PWM properties...
+const int freq = 30000;
+const int resolution = 8; // 8-bit resolution (0-255)
+int dutyCycle = 200;
+
+ 
 void setup() {
-  // these likely aren't the right pin names
-  // replace these with whatever pins i decide to use on the esp
-  pinMode(motorIn1, OUTPUT);
-  pinMode(motorIn2, OUTPUT);
-  pinMode(motorEn, OUTPUT);
+  // sets the pins as outputs:
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
   
-  ledcAttach(motorEn, 5000, 8);
+  // configure LEDC PWM directly on the pin (No channel or ledcSetup required)
+  ledcAttach(enable1Pin, freq, resolution);
+ 
+  Serial.begin(115200); // serial monitor
+
+  SerialBT.begin("Motor Test"); // start bt (motor test is device name)
 }
+ 
+void loop() {
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  } // who knows lowkey
 
-void loop() { // 'proof of concept' ahh, just untested filler essentially
-  // forward
-  digitalWrite(motorIn1, HIGH);
-  digitalWrite(motorIn2, LOW);
-  ledcWrite(motorEn, 200); // speed ranges from 0-255
-  delay(2000);
+  if (SerialBT.available() > 0) {
+    text = SerialBT.readStringUntil('\n'); 
+    text.trim(); // only get the actual usable stuff
 
-  // stop
-  digitalWrite(motorIn1, LOW);
-  digitalWrite(motorIn2, LOW);
-  delay(1000);
+    if (text == "Motor On"){ BT = CTL_ON; }
+    else if (text == "Motor Off"){ BT = CTL_OFF; }
+    else { BT = UNDEFINED; } // input unrecognised
+
+    switch (BT) { // match cases to outputs
+      case 0 : // motor on
+          Serial.println("Moving Forward");
+          digitalWrite(motor1Pin1, LOW);
+          digitalWrite(motor1Pin2, HIGH); 
+          ledcWrite(enable1Pin, 255); // Ensure enable pin is fully high
+        break;
+
+      case 1 : // motor off
+          Serial.println("Motor stopped");
+          digitalWrite(motor1Pin1, LOW);
+          digitalWrite(motor1Pin2, LOW);
+          ledcWrite(enable1Pin, 0); // Turn off PWM output
+        break;
+
+      case 2 : // undefined
+        Serial.println("undefined input");
+        break;
+    }
+  }
+
+  delay(20);
+ 
+  // // Stop the DC motor
+  // Serial.println("Motor stopped");
+  // digitalWrite(motor1Pin1, LOW);
+  // digitalWrite(motor1Pin2, LOW);
+  // ledcWrite(enable1Pin, 0); // Turn off PWM output
+  // delay(1000);
+ 
+  // // Move DC motor backwards at maximum speed
+  // Serial.println("Moving Backwards");
+  // digitalWrite(motor1Pin1, HIGH);
+  // digitalWrite(motor1Pin2, LOW); 
+  // ledcWrite(enable1Pin, 255);
+  // delay(2000);
+ 
+  // // Stop the DC motor
+  // Serial.println("Motor stopped");
+  // digitalWrite(motor1Pin1, LOW);
+  // digitalWrite(motor1Pin2, LOW);
+  // ledcWrite(enable1Pin, 0);
+  // delay(1000);
+ 
+  // // Move DC motor forward with increasing speed
+  // digitalWrite(motor1Pin1, HIGH);
+  // digitalWrite(motor1Pin2, LOW);
+  // while (dutyCycle <= 255){
+  //   // Target the pin directly instead of a channel
+  //   ledcWrite(enable1Pin, dutyCycle);   
+  //   Serial.print("Forward with duty cycle: ");
+  //   Serial.println(dutyCycle);
+  //   dutyCycle = dutyCycle + 5;
+  //   delay(500);
+  // }
+  // dutyCycle = 200;
 }
--- shell.nix
+++ shell.nix
@@ -11,13 +11,29 @@
     libusb1    
     pkg-config
     zlib
+    gnumake
+    gcc
+
+    # needed for compiling/uploading
+    python3
+    python3Packages.pyserial
     
-    llvmPackages.clang-tools     # required for arduino lsp (maybe not?)
+    llvmPackages.clang-tools # required for arduino lsp (maybe not?)
     arduino-language-server # lsp, duh
+
+    # ugh fucking ide might work
+    arduino-ide
+    xorg.libxkbfile
   ];
 
   # fucky directory stuff
   profile = ''
-    export ARDUINO_DATA_DIR="$HOME/.arduino15"
+    export HOME="/home/elaine"
+    export XDG_CONFIG_HOME="$HOME/.config"
+    export XDG_DATA_HOME="$HOME/.local/share"
+    
+    # export ARDUINO_DATA_DIR="$HOME/.arduino15"
+    export PATH="${pkgs.python3}/bin:$PATH"
+    export LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/current-system/sw/lib:${pkgs.xorg.libxkbfile}/lib:$LD_LIBRARY_PATH"
   '';
 }).env
